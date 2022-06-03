e283e7eb77d2a6f50a0d7f9044f191ad0c4fefe4
Kurniawan Sudirman <kurniawanplusayahibu@gmail.com>
2022-06-03 21:25:33 +0700

    Kurniawan Sudirman :completed

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, typeOfSort)=>{
+    let newNames = typeOfSort(names);
+    let newListNames = [];
+    for(let i = 0;i<newNames.length; i++) {
+      let revised = i+1 + ". " +names[i];
+      newListNames.push(revised);
+    }
+    return newListNames;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  return names.sort().reverse();
+
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
